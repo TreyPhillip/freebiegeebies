b4c4be16f5063c82b36f0bf1424bf60cb64fa33b
Trey Phillip <Treyphillip@live.com>
2021-04-01 15:30:55 -0400

    login form

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,41 @@
+import {useState} from 'react';
+import axios from 'axios';
+
+export default function Login() {
+    const [loginEmail, setLoginEmail] = useState("")
+    const [loginPassword, setLoginPassword] = useState("");
+    const login = () => {
+        //console.log(loginPassword, loginEmail)
+        axios({
+            method: "POST",
+            data: {
+                user_email: loginEmail,
+                user_password: loginPassword,
+            },
+            withCredentials: true,
+            url: "http://localhost:8080/login"
+        }).then((data) => console.log(data));
+    };
+
+    return (
+        <div>
+            <label>
+                Email:
+                <input 
+                    type="text" 
+                    value={loginEmail}
+                    onChange={e => setLoginEmail(e.target.value)}
+                />
+            </label><br/>
+            <label>
+                Password:
+                <input 
+                    type="password" 
+                    value={loginPassword}
+                    onChange={e => setLoginPassword(e.target.value)}
+                />
+            </label><br/>
+            <button onClick={login}>Submit</button>
+        </div>
+    );
+}

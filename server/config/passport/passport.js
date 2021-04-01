79e1c0687dcfe28fc773c9e8e4f036cff523fde9
Trey Phillip <Treyphillip@live.com>
2021-04-01 15:32:38 -0400

    fix authentication

--- server/config/passport/passport.js
+++ server/config/passport/passport.js
@@ -1,46 +1,42 @@
-var bCrypt = require('bcrypt-nodejs');
+const db = require("../../models");
+const User = db.users;
+const bcrypt = require("bcryptjs");
+const localStrategy = require("passport-local").Strategy
 
-module.exports = (passport, user) => {
-    var User = user;
-    var LocalStrategy = require('passport-local').Strategy;
-    
-    passport.use('local-signup', new LocalStrategy(
-        {
-            user_name_field: 'email',
-            user_password_field: 'password',
-            passReqToCallback: true
+module.exports = (passport) => {
+    passport.use(
+        new localStrategy({
+            usernameField: 'user_email',
+            passwordField: 'user_password'
         },
-        (req, email, password, done) => {
-            var generateHash = (password) => {
-                return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
-            };
-            User.findOne({
-                where: {
-                    email: email
-                }
-            }).then((user) => {
-                if (user) {
-                    return done(null, false, {
-                        message: 'That email is already in use'
-                    });
-                }
-                else {
-                    var user_password = generateHash(password);
-                    var data = {
-                        email: email,
-                        username: req.body.user_name,
-                        password: user_password
-                    };
-                    User.create(data).then = (newUser, created) => {
-                        if (!newUser) {
-                            return done(null, false);
-                        }
-                        if (newUser) {
-                            return done(null, newUser);
-                        }
+        async (user_email, user_password, done) => {
+            const user = await User.findOne({ where: {user_email: user_email} });
+            if (user === null) {
+                return done(null, false);
+            } else {
+                bcrypt.compare(user_password, user.user_password, (err, result) => {
+                    if (err) {
+                        throw err;
                     }
-                }
-            });
-        }   
-    ));
-}
+                    if (result === true) {
+                        return done(null, user);
+                    }
+                    else {
+                        return done(null, false);
+                    }
+                });
+            }
+        })
+    );
+    passport.serializeUser((user, cb) => {
+        cb(null, user.userID);
+    });
+    passport.deserializeUser((userID, cb) => {
+        User.findOne({ _userID: userID}, (err, user) => {
+            const userInformation = {
+                user_email: user.user_email,
+            };
+            cb(err, userInformation);
+        });
+    });
+};

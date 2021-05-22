61f8fc7c2319e4bd515a29118fc2293395f9f173
TreyPhillip <treyphillip@live.com>
2021-05-22 10:30:50 -0400

    add user image to model and database

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -18,6 +18,9 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.STRING,
             allowNull: false
         },
+        user_image_url: {
+            type: Sequelize.STRING
+        },
         verified: {
             type: Sequelize.BOOLEAN
         },

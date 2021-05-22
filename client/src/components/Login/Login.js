6b9006e06127fb94a2c6abf511aec380a57beb84
TreyPhillip <treyphillip@live.com>
2021-05-22 10:28:51 -0400

    enables authentication to be visible client side

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,9 +1,10 @@
-import {useState} from 'react';
+import React, { useState } from "react";
 import axios from 'axios';
 
-export default function Login() {
+export default function Login (props) {
     const [loginEmail, setLoginEmail] = useState("")
     const [loginPassword, setLoginPassword] = useState("");
+
     const login = () => {
         axios({
             method: "POST",
@@ -13,28 +14,36 @@ export default function Login() {
             },
             withCredentials: true,
             url: "http://localhost:8080/login"
-        }).then((res) => console.log(res));
+        }).then((res) => {
+            if (res.data === "No User Exists") {
+                props.setIsAuth(false);
+            }
+            if (res.data === "Successfully Authenticated") {
+                props.setIsAuth(true);
+            }
+            console.log(res.data);
+        });
     };
 
     return (
         <div>
             <label>
                 Email:
-                <input 
-                    type="text" 
+                <input
+                    type="text"
                     value={loginEmail}
-                    onChange={e => setLoginEmail(e.target.value)}
+                    onChange={(e) => setLoginEmail(e.target.value)}
                 />
             </label><br/>
             <label>
                 Password:
-                <input 
-                    type="password" 
+                <input
+                    type="password"
                     value={loginPassword}
                     onChange={e => setLoginPassword(e.target.value)}
                 />
             </label><br/>
             <button onClick={login}>Submit</button>
         </div>
-    );
+    )
 }

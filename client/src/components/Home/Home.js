c44e7d27e475449743046c7b22bd74077ae470ab
TreyPhillip <treyphillip@live.com>
2021-05-28 12:37:31 -0400

    update home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,24 +1,10 @@
-import axios from 'axios';
-import React, { useState } from "react";
-
+import './Home.css'
 
 export default function Home() {
-    const [data, setData] = useState(null);
-    const getUser = () => {
-        axios({
-            method: "GET",
-            withCredentials: true,
-            url: "http://localhost:8080/user",
-        }).then((res) => {
-            setData(res.data);
-            //console.log("userdata: " + res);
-        });
-    }
+
     return(
-        <main>
-            <h1>Get User</h1>
-            <button onClick={getUser}>Submit</button>
-            {data ? <h1>Welcome Back {data.user_name}</h1> : null}
-        </main>
+        <div>
+            <h1>Welcome Home</h1>
+        </div>
     );
 };
